5b8a527d2241ed4d7980501843239e4eb88b8d1d
Vladislav Zubakin <shaqimusic@gmail.com>
2022-03-31 20:34:38 +0300

    refactor: code refactoring

--- src/components/ui-elements/forms/checkbox-with-label.jsx
+++ src/components/ui-elements/forms/checkbox-with-label.jsx
@@ -1,12 +1,24 @@
-import * as React from 'react';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+import * as React from "react";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 
-export default function CheckboxWithLabel({label}) {
-  return (
-    <FormGroup>
-      <FormControlLabel control={<Checkbox />} label={label} />
-    </FormGroup>
-  );
+export default function CheckboxWithLabel({ label }) {
+	const [checked, setChecked] = React.useState(false);
+
+	const handleChange = (event) => {
+		setChecked(event.target.checked);
+	};
+
+	return (
+		<FormGroup>
+			<FormControlLabel
+				control={<Checkbox
+          onChange={handleChange}
+          inputProps={{ 'aria-label': 'controlled' }}
+        />}
+				label={label}
+			/>
+		</FormGroup>
+	);
 }
