637c714c161a2ee12489ec357d401dd73acb5d9c
Vladislav Zubakin <shaqimusic@gmail.com>
2022-03-31 14:09:11 +0300

    feat: add services

---
+++ src/services/index.js
@@ -0,0 +1,11 @@
+import BinanceService from "./binance-service";
+import getLexxLink from './get-lexx-link';
+import backtestService from "./backtest-service"
+import createOptions from "./create-options"
+
+export {
+    BinanceService,
+    getLexxLink,
+    backtestService,
+    createOptions
+}
